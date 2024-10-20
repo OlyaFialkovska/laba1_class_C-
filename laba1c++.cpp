26debd8024b8b2724e8bd06e398fc80547c61d21
Olya <106016782+OlyaFialkovska@users.noreply.github.com>
2024-10-20 16:10:11 +0300

    Add files via upload

--- laba1c++.cpp
+++ laba1c++.cpp
@@ -47,23 +47,28 @@ void calculationAngleBetweenTwoVectors()
 }
 
 class vector2d {
+    private:
+    float x,y;
     public:
-    float a,b,c;
-    vector2d(): a(0.0f),b(0.0f),c(0.0f)//default
+    vector2d(): x(0.0f),y(0.0f)//default
     {
         //...
     }
     vector2d(const vector2d &another)://copying
-    a(another.a),
-    b(another.b),
-    c(another.c)
+    x(another.x),
+    y(another.y)
     {
         //...
     }
-    vector2d(float i,float k,float j)://initialization with a pair of real numbers
-    a(i), b(k),c(j)
+    vector2d(float i,float j)://initialization with a pair of real numbers
+    x(i), y(j)
     {
         //...
+    }
+
+    void out() const 
+    {
+        cout << "x = " << x << " y = " << y  << "\n";
     }
 
 
@@ -77,7 +82,11 @@ int main()
 {
     vector2d constructorDefault;
     vector2d constructorcopy(constructorDefault);
-    vector2d constructoranother(3.5,6.7,8.9);
+    vector2d constructoranother(3.5,8.9);
+
+    constructorDefault.out();
+    constructorcopy.out();
+    constructoranother.out();
 
     return 0;
 }
